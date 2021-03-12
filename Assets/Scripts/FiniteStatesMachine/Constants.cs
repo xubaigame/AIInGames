649e3b719441f689bc80167997d1c1d432436db2
Mumu <>
2021-03-12 23:45:47 +0800

    二、寻路算法 寻路算法代码框架搭建

---
+++ Assets/Scripts/FiniteStatesMachine/Constants.cs
@@ -0,0 +1,23 @@
+// ****************************************************
+//     文件：Constants.cs
+//     作者：积极向上小木木
+//     邮箱：positivemumu@126.com
+//     日期：2021/3/12 23:37:53
+//     功能：
+// *****************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    //Transitions
+    public static string MissPlayer = "MissPlayer";
+    public static string LookPlayer = "LookPlayer";
+    
+    
+    //StateNames
+    public static string PatrolState = "PatrolState";
+    public static string FollowPlayerState = "FollowPlayerState";
+}
